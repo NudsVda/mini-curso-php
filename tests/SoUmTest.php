d6616cd0694e6536f9a4db1155bc8dd5aca3146d
Tiago Heineck <tiago.heineck@ifc.edu.br>
2021-11-23 11:30:38 -0300

    adiciona casos de testess

---
+++ tests/SoUmTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SoUmTest extends TestCase
+{
+    
+    public function testDoTeste()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+        return $stack;
+    }
+
+}
